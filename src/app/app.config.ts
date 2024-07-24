3fbde3ad8f3189ad224d33ce9170d922a89ecf99
Tomislav Kaljević <105347909+tkaljevic@users.noreply.github.com>
2024-07-24 16:09:26 +0200

    #2 - Angular material added (#6)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { httpErrorInterceptor, httpLoaderInterceptor } from '@core-interceptors';
 import { routes } from './app.routes';
 
@@ -13,5 +14,6 @@ export const appConfig: ApplicationConfig = {
       httpLoaderInterceptor,
       httpErrorInterceptor
     ])),
+    provideAnimationsAsync(),
   ]
 };
