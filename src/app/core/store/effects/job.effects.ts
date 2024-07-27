f83079bb4ab9b9495bda8aea5ff6b2ff0713ad50
Tomislav Kaljevic <tomislav.kaljevic@qposoft.com>
2024-07-27 02:45:42 +0200

    #6 - bugfix - delete last item

--- src/app/core/store/effects/job.effects.ts
+++ src/app/core/store/effects/job.effects.ts
@@ -42,22 +42,41 @@ export class JobEffects {
     this.actions$.pipe(
       ofType(JobActions.deleteJobAction),
       withLatestFrom(
-        this.store.pipe(select(JobSelectors.currentPaginationSelector))
+        this.store.pipe(select(JobSelectors.currentPaginationSelector)),
+        this.store.pipe(select(JobSelectors.jobsPaginationSelector))
       ),
-      mergeMap(([action, pagination]) =>
+      mergeMap(([action, pagination, metaPagination]) =>
         this.jobsHttpService.deleteJob(action.jobId).pipe(
           tap((deletedJob) =>
             this.toasterService.showSuccess(
               `Successfully deleted ${deletedJob.title}`
             )
           ),
-          concatMap(() => [
-            JobActions.deleteJobSuccessAction(),
-            JobActions.loadJobsAction({
-              page: pagination.page,
-              perPage: pagination.perPage,
-            }),
-          ]),
+          concatMap(() => {
+            if (
+              metaPagination.next === null &&
+              metaPagination.items === pagination.perPage + 1
+            ) {
+              return [
+                JobActions.setCurrentPagination({
+                  page: pagination.page - 1,
+                  perPage: pagination.perPage,
+                }),
+                JobActions.deleteJobSuccessAction(),
+                JobActions.loadJobsAction({
+                  page: pagination.page - 1,
+                  perPage: pagination.perPage,
+                }),
+              ];
+            }
+            return [
+              JobActions.deleteJobSuccessAction(),
+              JobActions.loadJobsAction({
+                page: pagination.page,
+                perPage: pagination.perPage,
+              }),
+            ];
+          }),
           catchError((error) =>
             of(JobActions.deleteJobFailureAction({ error: error.message }))
           )
